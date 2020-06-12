6d90473b2812603d7ef297dcf5fab2cfbf2a88fc
Mungai <elias.karume@yahoo.com>
2020-06-13 00:07:51 +0300

    HomeGuest changes

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -194,8 +194,8 @@ function HomeGuest() {
     <Page title="Welcome!" wide={true}>
       <div className="row align-items-center">
         <div className="col-lg-7 py-3 py-md-5">
-          <h1 className="display-3">Remember Writing?</h1>
-          <p className="lead text-muted">Are you sick of short tweets and impersonal &ldquo;shared&rdquo; posts that are reminiscent of the late 90&rsquo;s email forwards? We believe getting back to actually writing is the key to enjoying the internet again.</p>
+          <h1 className="display-3">React Blog App</h1>
+          <p className="lead text-muted">A simple react blog app. SignUp for an account, create and post your ideas or posts, follow some of your friends and chats with them. </p>
         </div>
         <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
           <form onSubmit={handleSubmit}>
